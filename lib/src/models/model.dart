c874c12d5e4d66db1c8f577f82c2932dce6a85e3
Sarawut Kengkan <sarawut.ken@axonstech.com>
2023-11-05 19:08:03 +0700

    feat: add model comment

--- lib/src/models/model.dart
+++ lib/src/models/model.dart
@@ -1,3 +1,4 @@
 export 'package:flutter_state_management/src/models/todo.dart';
 export 'package:flutter_state_management/src/models/post.dart';
 export 'package:flutter_state_management/src/models/user.dart';
+export 'package:flutter_state_management/src/models/comment.dart';
